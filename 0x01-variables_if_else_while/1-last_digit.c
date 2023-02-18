ceb4cfb450c04049ba42d55377c081ed6c64a132
Samuel Otun <otunsamuel47@gmail.com>
2023-02-17 17:33:50 -0800

    This program will assign a random number to the variable n each time it is executed.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,10 +9,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
+ *main - Prints the last digit of a randomly generated number
  *	  and whether it is greater than 5, less than 6, or 0. 
  *  
- * Return: always 0.
+ *Return: always 0.
 */
 int main(void)
 {
