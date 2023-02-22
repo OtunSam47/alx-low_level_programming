e16117f3844772de2f833382800566cba9e11682
Samuel Otun <otunsamuel47@gmail.com>
2023-02-21 19:38:31 -0800

    Header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containing declarations for all functions
+ *       used in this current directory.
+ */
+
+int _putchar(char c);
+
+#endif
