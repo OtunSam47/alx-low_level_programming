69460a52e1db92b92bc0cc69f0353be9689b41b8
Samuel Otun <otunsamuel47@gmail.com>
2023-02-23 09:08:31 -0800

    2-mul.c task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for uppercase characters.
+ * @a and @b: The characters to be checked.
+ *
+ * Return: 0
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
