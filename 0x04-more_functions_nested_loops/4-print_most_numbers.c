285223a1c5bd6c0383584062c5c52dfd777388d6
Samuel Otun <otunsamuel47@gmail.com>
2023-02-23 09:58:21 -0800

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 9; dig++)
+	{
+		if (dig = 2 || dig = 4)
+			continue;
+		else
+			_putchar((dig % 10) + '0');
+	}
+
+	_putchar('\n');
+}
